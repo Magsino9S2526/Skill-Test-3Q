a4fe4935ac0bd9fd15b634fe2b5a29c3eb61a598
FiShNcHiPs <magsino.mh@obmontessori.edu.ph>
2026-03-16 22:37:22 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function computeTotal(){
-    // get quantity value
-  let qty = document.getElementById("qty").value;
-    // calculate subtotal
-    let subtotal = qty * price;
-    // calculate total
-    let total = subtotal + adminFee;
-    // display results
-    document.getElementById("subtotal").innerHTML = subtotal;
-    document.getElementById("total").innerHTML = total;
+function verbnoun{
+}
+value= 0 min= 0
+//variable delcaration
+const ticketprice= 250;
+const adminfee= 50.00;
+let ticketqty= number (document.getElementByIddocumment)().value
+let subtotal= number
+onclick="vernoun"
+let toalprice=number
+
+function showBrands() {
+  brandPanel.style.display = "block";
+  storePanel.style.display = "none";
+
+  calculateDiscount();
+}
+
+function showStore() {
+  storePanel.style.display = "block";
+  brandPanel.style.display = "none";
+}
+
+function calculateDiscount() {
+  const budget = Number(budgetInput.value);
+
+  // Example: 20% discount
+  const discount = budget * 0.2;
+
+  discountValue.textContent = discount.toFixed(2);
+}
